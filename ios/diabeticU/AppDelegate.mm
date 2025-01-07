19aed512ef219ad30389c4ca8dadb38526d45e45
Sejal Srivastava <85271912+Sejal99@users.noreply.github.com>
2025-01-07 22:34:31 +0530

    Update AppDelegate.mm

--- ios/diabeticU/AppDelegate.mm
+++ ios/diabeticU/AppDelegate.mm
@@ -6,7 +6,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-
   self.moduleName = @"diabeticU";
   
   // You can add your custom initial props in the dictionary below.
